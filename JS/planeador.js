07f42c0bba4ff1ecc4aa4a0c84deeeed6f4e1d33
Adrian Gonzalez <adrianncba2@gmail.com>
2024-12-29 19:11:52 -0300

    modificacion git commit -m modificacion

--- JS/planeador.js
+++ JS/planeador.js
@@ -174,7 +174,7 @@ function cargarGastos() {
     }
 
  // Usamos fetch para cargar los datos de gastos desde gastos.json
-    fetch("../JS/gastos.json")
+    fetch("./gastos.json")
         .then(response => {
             if (!response.ok) {
                 throw new Error('Error al cargar el archivo JSON');
